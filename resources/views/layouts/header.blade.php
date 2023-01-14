445bdc8f625c716b07ff01f28f9c82de3593b6fa
JuanGabriel <juangabrielrinas@gmail.com>
2023-01-14 18:35:34 -0300

    first commit

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,12 @@
+<header class="header-content">
+    @include('layouts.nav1')
+
+    <div class="header-presentation">
+        <h1>Juan Gabriel Benitez Rinas</h1>
+        <h4>Ciberseguridad</h4>
+
+        <a href="#">Contáctame</a>
+    </div>
+
+
+</header>
---
+++ resources/views/layouts/nav1.blade.php
@@ -0,0 +1,11 @@
+<nav class="nav1-header">
+    <h1>Juan Gabriel Rinas</h1>
+    <ul>
+        <li><a href="">Inicio</a></li>
+        <li><a href="">Sobre mi</a></li>
+        <li><a href="">Habilidades</a></li>
+        <li><a href="">Videos</a></li>
+        <li><a href="">Proyectos</a></li>
+        <li><a href="">Contacto</a></li>
+    </ul>
+</nav>
