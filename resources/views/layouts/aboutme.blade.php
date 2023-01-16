1c23f04544c8d89c592acd3a2a3320c1e4289979
JuanGabriel <juangabrielrinas@gmail.com>
2023-01-16 15:03:53 -0300

    about me finalizado

--- resources/views/layouts/aboutme.blade.php
+++ resources/views/layouts/aboutme.blade.php
@@ -1,27 +1,56 @@
-<section class="aboutme">
+<section class="aboutme" id="aboutme">
 
-      <article class="aboutme-item">
+    <article class="aboutme-item">
         <div class="aboutme-img">
-            <img src="{{ asset('assets/images/fotobien.png'); }}" alt="">
+            <span></span>
+            <img src="{{ asset('assets/images/fotobien.png') }}" alt="">
         </div>
-        
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, quisquam repellendus. Molestiae assumenda repellat ratione itaque, repellendus possimus veritatis fugit veniam iste maxime explicabo incidunt, omnis earum libero quod quisquam.</p>
-      </article>
 
-      <article class="aboutme-item">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita sint fugiat nulla quod alias quaerat sapiente veritatis voluptatem unde. Enim, voluptatum nisi cum blanditiis eum dolores nobis eveniet perferendis necessitatibus?</p>
-      </article>
+        <p>Me llamo Juan Gabriel Benítez Rinas. Soy estudiante de la carrera Licenciatura en sistemas de la información en la UNNE. En
+            paralelo a las materias de la carrera me gusta estudiar cosas sobre desarrollo web y todo lo relacionado con
+            la seguridad informática, enfocándome principalmente en esta última. </p>
+    </article>
 
-      <article class="aboutme-item">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita sint fugiat nulla quod alias quaerat sapiente veritatis voluptatem unde. Enim, voluptatum nisi cum blanditiis eum dolores nobis eveniet perferendis necessitatibus?</p>
-      </article>
+    <article class="aboutme-item">
 
-      <article class="aboutme-item">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita sint fugiat nulla quod alias quaerat sapiente veritatis voluptatem unde. Enim, voluptatum nisi cum blanditiis eum dolores nobis eveniet perferendis necessitatibus?</p>
-      </article>
+        <div class="datosP">
+            <h2>DATOS PERSONALES</h2>
+            <div class="aboutme-datosP">
+                <p class="material-symbols-outlined icon">call <span>+543794560224</span></p>
+                <p class="material-symbols-outlined icon">mail <span>juangabrielrinas@gmail.com</span></p>
+                <p class="material-symbols-outlined icon">calendar_month <span>21/01/2001</span></p>
+                <p class="material-symbols-outlined icon">pin_drop <span>Corrientes | Argentina</span></p>
+            </div>
 
-      <article class="aboutme-item">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita sint fugiat nulla quod alias quaerat sapiente veritatis voluptatem unde. Enim, voluptatum nisi cum blanditiis eum dolores nobis eveniet perferendis necessitatibus?</p>
-      </article>
+        </div>
+
+        <div class="datosP">
+            <h2>FORMACIÓN <span class="line"></span></h2>
+            <div class="aboutme-datosP">
+                <p class="material-symbols-outlined icon">school <span>Licenciatura en sistemas de la información |
+                        Unne</span></p>
+                <p class="material-symbols-outlined icon">history_edu <span>Analista programador universitario |
+                        Pregrado</span></p>
+                <p class="material-symbols-outlined icon">horizontal_rule <span>2020 / en curso</span></p>
+                <p class="material-symbols-outlined icon">history_edu <span>Licenciado en sistemas | Grado</span></p>
+                <p class="material-symbols-outlined icon">horizontal_rule <span>2020 / en curso</span></p>
+            </div>
+
+        </div>
+
+        <div>
+            <h2>SKILLS <span class="line"></span></h2>
+            <div class="aboutme-skills" id="skills">
+                <div><img src="{{ asset('assets/icons/kalilinux.png') }}" alt=""></div>
+                <div><img src="{{ asset('assets/icons/js.png') }}" alt=""></div>
+                <div><img src="{{ asset('assets/icons/c-sharp.png') }}" alt=""></div>
+                <div><img src="{{ asset('assets/icons/mysql.png') }}" alt=""></div>
+                <div><img src="{{ asset('assets/icons/sql.png') }}" alt=""></div>
+                <div><img src="{{ asset('assets/icons/php.png') }}" alt=""></div>
+            </div>
+        </div>
+    </article>
+
+
 
 </section>
--- resources/views/layouts/nav1.blade.php
+++ resources/views/layouts/nav1.blade.php
@@ -2,8 +2,8 @@
     <h1>Juan Gabriel Rinas</h1>
     <ul>
         <li><a href="#inicio">Inicio</a></li>
-        <li><a href="">Sobre mi</a></li>
-        <li><a href="">Habilidades</a></li>
+        <li><a href="#aboutme">Sobre mi</a></li>
+        <li><a href="#skills">Habilidades</a></li>
         <li><a href="">Videos</a></li>
         <li><a href="">Proyectos</a></li>
         <li><a href="">Contacto</a></li>
